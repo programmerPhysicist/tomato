88ebf08e6ed40e7e74f11c2861b0c3f095836624
Alexander Jamison Marvin <ajmarvin23@gmail.com>
2016-11-02 11:35:11 -0600

    Improved the appearance of the text output.

--- tomato.cpp
+++ tomato.cpp
@@ -4,7 +4,7 @@
  * Author:
  *    Alexander Marvin
  * Summary:
- *    A timer
+ *    A Pomodoro timer command line application
  ************************************************************************/
 #include <iostream>
 #include <cstdlib>
@@ -19,21 +19,22 @@ void playAlarm();
 int main()
 {
   //initialize stuff
-  settings config;
-  config.loadConfig(CONFIG_FILE);
+  settings config; //initialize settings object
+  config.loadConfig(CONFIG_FILE); //load settings into config
   bool success;
-  while(true)
+  while(true) //loop forever, or until user
     {
-      for (int i = 0; i < 4; i++)
+      for (int i = 0; i < 4; i++) //loop thru 4 pomodoros
 	{
-	  cout << "Time to work!\n";
+	  cout << endl << "\t**Time to work!**\n";
 	  playAlarm();
 	  success = timer(config.workTime);
-	  cout << "Breaktime!\n";
+	  cout << endl << "\t**Breaktime!**\n";
 	  playAlarm();
 	  success = timer(config.shortBreak);
 	}
-      cout << "Time for long break!\n";
+      //then long break
+      cout << "\t**Time for long break!**\n";
       playAlarm();
       success = timer(config.longBreak);
     }
@@ -42,19 +43,20 @@ int main()
 
 bool timer(int minutes)
 {
-  int waitTime = 60*SECOND;
-  for (int i = 0; i < minutes; i++)
+  int waitTime = 60*SECOND; //one minute
+  for (int i = 0; i < minutes; i++) //loop thru minutes
     {
       //display
       cout << endl;
-      cout << (minutes - i) << " minutes remaining\n";
+      cout << "\tTime left:" << (minutes - i) << "m" << endl;
       //wait
-      WAIT(waitTime);
+      WAIT(waitTime); //wait one minute
     }
   return true;
 }
 
 void playAlarm()
 {
+  //play ringing sound
   system("paplay 'sounds/alarm_tones/ringing.wav'");
 }
