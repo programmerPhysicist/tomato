24c0ecb5e469627a06de415cabd649213e99e5aa
Alex Marvin <ajmarvin23@gmail.com>
2016-11-02 14:18:57 -0600

    Add files via upload
    Modify to allow it to compile on Windows.

--- tomato.h
+++ tomato.h
@@ -1,37 +1,68 @@
 /***********************************************************************
- * Header File:
- *    tomato.h
- * Author:
- *    Alexander Marvin
- * Summary:
- *    Header file for tomato program
- ************************************************************************/
+ 
+* Header File:
+ 
+*   tomato.h
+ 
+* Author:
+
+*   Alexander Marvin
+ 
+* Summary:
+ 
+*   Header file for tomato program
+ 
+************************************************************************/
+
 #ifndef TOMATO_H
+
 #define TOMATO_H
 
+
 /******************Choose between operating systems**********************
-*************************************************************************/
+**************
+***********************************************************/
+
 
 /****************************WINDOWS*************************************/
+
 #ifdef _WIN32
 #include <windows.h>
-//#include "tchar.h"
+//
+#include "tchar.h"
+
 #define WAIT Sleep
+
 #define SECOND 1000
+
+#define CONFIG_FILE ".tomato"
 /**************************UNIX OR LINUX*********************************/
+
 #elif defined(__unix__) || defined(__linux__)
+
 #include <ctime>
+
 #include <unistd.h>
+
 #define LINUX_UNIX
+
 #define WAIT usleep
+
 #define SECOND 1000000
 
+
 //define name/directory for config file
+
 #ifndef NDEBUG
+
 #define CONFIG_FILE ".tomato"
+
 #endif // define config file
 
+
 #endif // ENDIF for operating system choice
-/************************************************************************
- ************************************************************************/
+
+/*********************************************************
+***************************************************************************************/
+
 #endif // TOMATO_H
