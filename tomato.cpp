a0c5826be5aa79120e950d86ffd9f4fc029eeb89
Alexander Jamison Marvin <ajmarvin23@gmail.com>
2016-11-11 00:07:04 -0700

    Keep time on same line while timer is running.

--- tomato.cpp
+++ tomato.cpp
@@ -23,6 +23,7 @@ int main()
   settings config; //initialize settings object
   config.loadConfig(CONFIG_FILE); //load settings into config
   bool success;
+  //cout << "0x9B 0x3F 0x32 0x35 0x6C";
   while(true) //loop forever, or until user
     {
       for (int i = 0; i < 4; i++) //loop thru 4 pomodoros
@@ -33,6 +34,7 @@ int main()
 	  cout << endl << "**Breaktime!**\n";
 	  playAlarm();
 	  success = timer(config.shortBreak);
+	  cout << endl;
 	}
       //then long break
       cout << "**Time for long break!**\n";
@@ -45,14 +47,16 @@ int main()
 bool timer(int minutes)
 {
   int waitTime = 60*SECOND; //one minute
+  cout << "Time left:";
   for (int i = 0; i < minutes; i++) //loop thru minutes
     {
       //display
-      //cout << "\033[2K";
-      cout << "Time left: ";
-      cout << setw(3) << (minutes - i) << "m" << endl;;
+      //display time in minutes
+      cout << setw(3) << (minutes - i) << "m" << flush;
       //wait
       WAIT(waitTime); //wait one minute
+      //move cursor back to write new time
+      cout << "\033[4D";
     }
   return true;
 }
