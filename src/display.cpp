340198b03e2fde692ca326e086b6e100152bc2b4
Alexander Jamison Marvin <ajmarvin23@gmail.com>
2017-04-29 21:47:46 -0600

    Make things more compact.

--- src/display.cpp
+++ src/display.cpp
@@ -21,17 +21,17 @@ display::display()
   refresh(); //Now refresh screen
 
   //setup top bar
-  topBar = newwin(1, 80, 0, 0); //create new window
-  wattron(topBar, A_UNDERLINE); //text is to be underlined
+  topBar = newwin(1, 20, 0, 0); //create new window
+  //box(topBar, 0, 0);
 
   //setup timer window
-  timerWin = newwin(22, 80, 1, 0);
+  timerWin = newwin(3, 20, 1, 0);
   box(timerWin, 0, 0); //drawoutline of window
   setClock("00:00"); //draw clock
   wrefresh(timerWin); //refresh timer window
   
   //setup bottom bar
-  bottomBar = newwin(1, 80, 23, 0); //create new window
+  bottomBar = newwin(1, 30, 4, 0); //create new window
   keypad(bottomBar, TRUE); //setup to receive input from user
   wrefresh(bottomBar); //refresh
 }
@@ -40,17 +40,19 @@ void display::displayMessage(string message)
 {
   //do some other necessary stuff
   int length = message.length(); //get length of message
+  
   wmove(topBar, 0, 0); //move curser back to beginning
   
-  wmove(topBar, 0, 40 - length/2 - 1); //move cursor to center text
+  //wmove(topBar, 0, 40 - length/2 - 1); //move cursor to center text
   wprintw(topBar,message.c_str()); //print message
   wrefresh(topBar); //refresh
 }
 
 void display::setClock(string time)
 {
-  wmove(timerWin, 1, 1); //move curser to location
-  wclrtoeol(timerWin); //erase line
+  wmove(timerWin, 1, 1); //move curser to start of line
+  wprintw(timerWin, "     "); //erase line
+  wmove(timerWin, 1, 1); //move curser back
   wprintw(timerWin, time.c_str()); //print
   wrefresh(timerWin); //refresh
 }
@@ -60,6 +62,7 @@ char display::getUserInput(string message)
   curs_set(2); //make cursor visible
   char a;
   wmove(bottomBar, 0, 0); //move below line
+  wclrtoeol(bottomBar); //erase line
   wprintw(bottomBar, message.c_str()); //print prompt
   wrefresh(bottomBar); //refresh
   a = wgetch(bottomBar); //get message
